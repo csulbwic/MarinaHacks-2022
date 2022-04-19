a41984d7b46f4cbb24be081ccc708541aa61d71c
githelsui <githelsuico@gmail.com>
2022-04-18 18:22:35 -0700

    parallax + landing section done

--- js/main.js
+++ js/main.js
@@ -122,11 +122,11 @@ window.addEventListener('scroll', () => {
   let children = parent.getElementsByTagName('div');
   for(let i = 0; i < children.length; i++) {
     //skip foreground layer
-      if (i != children.length - 1 && i != 3){
+      if (i != children.length - 1 && i != 1){
         children[i].style.transform = 'translateY(-' + (window.pageYOffset * i / children.length ) + 'px)';
       } else if (i == children.length - 1){ //foreground layer-account for About Section
         children[i].style.transform = 'translateY(-' + (window.pageYOffset / children.length ) + 'px)';
-      } else if (i == 3) { // light png - moves vertically
+      } else if (i == 1) { // light png - moves vertically
         children[i].style.transform = 'translateX(' + (window.pageYOffset * i / children.length ) + 'px)';
       }
     }
